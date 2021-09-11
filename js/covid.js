4d6a4ea38062876262d8409c6a7c829ce5f5bbdf
Athoscaetano <Athosalizon1993@gmail.com>
2021-09-11 13:07:23 -0300

    Criado o código javascript para a validação de nome e email.

--- js/covid.js
+++ js/covid.js
@@ -1,19 +1,16 @@
-<script>
-function verifica() {
-  if (document.forms[0].email.value.length == 0) {
-    alert('Por favor, informe o seu EMAIL.');
-	document.frmEnvia.email.focus();
-    return false;
+function enviarDados() {
+  function validmail(email) {
+      if (email.indexOf("@") == -1 || email == "" || email.indexOf(".") == - 1) {
+          alert("Email incorreto, preencha corretamente o campo.");
+          email.focus();
+          return false;
+      }
   }
-  return true;
+  var nome = document.getElementById("nome").value;
+  if (nome.length < 3) {
+      alert("O nome deve ter no mínimo 3 letras.");
+      nome.focus();
+      return false;
+  }
+  var recebeEmail = validmail(document.getElementById("email").value);
 }
-function checarEmail(){
-if( document.forms[0].email.value=="" 
-   || document.forms[0].email.value.indexOf('@')==-1 
-     || document.forms[0].email.value.indexOf('.')==-1 )
-	{
-	  alert( "Por favor, informe um E-MAIL válido!" );
-	  return false;
-	}
-}
-</script>
