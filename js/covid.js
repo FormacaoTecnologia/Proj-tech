1bba70c97e7ddc8b60c552c11a89fcd4e31856f5
brunoserra <brunorrrossi@gmail.com>
2021-09-12 17:51:37 -0300

    css parte primaria
    parte basíca finalixada falta alguns ajsutes no formulãrio

--- js/covid.js
+++ js/covid.js
@@ -1,16 +1,16 @@
 function enviarDados() {
-  function validmail(email) {
-      if (email.indexOf("@") == -1 || email == "" || email.indexOf(".") == - 1) {
-          alert("Email incorreto, preencha corretamente o campo.");
-          email.focus();
-          return false;
-      }
-  }
-  var nome = document.getElementById("nome").value;
-  if (nome.length < 3) {
-      alert("O nome deve ter no mínimo 3 letras.");
-      nome.focus();
-      return false;
-  }
-  var recebeEmail = validmail(document.getElementById("email").value);
+    function validmail(email) {
+        if (email.indexOf("@") == -1 || email == "" || email.indexOf(".") == -1) {
+            alert("Email incorreto, preencha corretamente o campo.");
+            email.focus();
+            return false;
+        }
+    }
+    var nome = document.getElementById("nome").value;
+    if (nome.length < 3) {
+        alert("O nome deve ter no mínimo 3 letras.");
+        nome.focus();
+        return false;
+    }
+    var recebeEmail = validmail(document.getElementById("email").value);
 }
