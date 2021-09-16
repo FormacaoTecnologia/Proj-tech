e399376aa4f70ee80ef156c231ddf29068986072
Athoscaetano <Athosalizon1993@gmail.com>
2021-09-16 12:34:38 -0300

    covid.js

--- js/covid.js
+++ js/covid.js
@@ -1,16 +1,28 @@
-function enviarDados() {
-    function validmail(email) {
-        if (email.indexOf("@") == -1 || email == "" || email.indexOf(".") == -1) {
-            alert("Email incorreto, preencha corretamente o campo.");
-            email.focus();
-            return false;
-        }
-    }
-    var nome = document.getElementById("nome").value;
-    if (nome.length < 3) {
-        alert("O nome deve ter no mínimo 3 letras.");
-        nome.focus();
-        return false;
-    }
-    var recebeEmail = validmail(document.getElementById("email").value);
-}
+
+//Aqui eu criei a função validar.
+function validar() {            
+    //Depois, chamei os elementos por id.
+    var nome=document.getElementById("nome");
+                var email=document.getElementById("email");
+                var genero = document.getElementById("genero");
+                //Aqui utilizei condicionais se os campos estiverem vazios, utilizando posteriormente alertas de erro. 
+                               if(nome.value == "") {
+                    alert("nome não informado");
+                    //Aqui dei foco nos elementos e usei um retorno para parar a execução  
+                                      nome.focus();
+                    return;
+                }
+                if(email.value == "") {
+                    alert("email não informado");
+                    email.focus();
+                    return;                                
+                     }  
+                     if(genero.value == "") {
+                         alert("gênero não informado");
+                         genero.focus();
+                         return;
+                     }
+      //Aqui um alerta caso o formulário seja preenchido corretamente e o comando para submeter o formulário.
+      alert("formulario enviado com sucesso!");
+      //formulario.submit();               
+                                   }
